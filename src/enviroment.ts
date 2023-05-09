1d24f7526cc65446e12b52db8001e5ba36d92c28
Mateus Galvão de Camargo <mateus.galvao.camargo.contato@gmail.com>
2023-05-09 14:54:46 -0300

    taca vai

---
+++ src/enviroment.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApmg52v2GMZ3n52zSkmj2s_iPusLjVXVc",
+  authDomain: "crud-angular-firebase-1227b.firebaseapp.com",
+  databaseURL: "https://crud-angular-firebase-1227b-default-rtdb.firebaseio.com",
+  projectId: "crud-angular-firebase-1227b",
+  storageBucket: "crud-angular-firebase-1227b.appspot.com",
+  messagingSenderId: "933377656849",
+  appId: "1:933377656849:web:ba788d2b96fd702d2389b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
